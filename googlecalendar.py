4179d0fe1bc3174121614caf7d7f56371eba7568
Koube Tomoyuki <koube@Koube-no-iMac.local>
2015-06-01 12:07:44 +0900

    update

--- googlecalendar.py
+++ googlecalendar.py
@@ -1,55 +1,54 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 import os
 
 from httplib2 import Http
 import oauth2client
-import gflags
-from oauth2client import tools
 from apiclient.discovery import build
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.tools import run_flow
 from oauth2client.file import Storage
 
-SCOPES = 'https://www.googleapis.com/auth/calendar'
-CLIENT_SECRET_FILE = 'client_secret.json'
+	
+class GoogleCalendar:
+	def __init__(self, userid):
+		home_dir		= os.path.expanduser('~')
+		credential_dir	= os.path.join(home_dir, '.credentials')
+		credential_path	= os.path.join(credential_dir, 'calendar.json')
 
-def get_credentials():
+	    #if not os.path.exists(credential_dir):
+		
+		store 			= oauth2client.file.Storage(credential_path)
+		credentials		= store.get()
+		self.service	= build('calendar', 'v3', http=credentials.authorize(Http()))
 
-	try:
-	    import argparse
-	    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-	except ImportError:
-		flags = None
+	def getCalendarId(self, calendarStr):
 
-	home_dir 		= os.path.expanduser('~')
-	credential_dir	= os.path.join(home_dir, '.credentials')
+		calendars	= self.service.calendarList().list().execute()
+		for calendar in calendars['items']:
+			if calendar['summary'] == calendarStr:
+				return calendar['id'].encode("utf-8")
 
-	if not os.path.exists(credential_dir):
-		os.makedirs(credential_dir)
+	def getScheduleList(self, calId):
 
-	credential_path	= os.path.join(credential_dir, 'calendar.json')
-
-	store 			= oauth2client.file.Storage(credential_path)
-	credentials		= store.get()
-
-	if not credentials or credentials.invalid:
-		flow			= flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-		flow.user_agent	= "test_application"
-		credentials 	= run_flow(flow, store, flags)
-
-		print 'Storing credentials to ' + credential_path
-	return credentials
-
-def main():
-	credentials = get_credentials()
-	service		= build('calendar', 'v3', http=credentials.authorize(Http()))
-
-	calendars	= service.calendarList().list().execute()
-	for calendar in calendars['items']:
-		print "%s, %s" % (calendar['id'], calendar['summary'])
+		events	= self.service.events().list(	calendarId = calId,
+												timeMin = '2015-05-01T00:00:00Z',
+												timeMax = '2015-05-30T23:59:59Z',
+												singleEvents = True
+												).execute()
+		for event in events['items']:
+			print "%s, %s [%s] - [%s]" % (	event['summary'],
+											event["start"]["date"],
+											event["start.dateTime"],
+											event["end.dateTime"])
+											
+		
 
 if __name__ == '__main__':
-	main()
+	str	= u"ビジネス（予定）"
 
+	gc	= GoogleCalendar("t.koube")
+	id	= gc.getCalendarId(str);
+	print id
+	gc.getScheduleList(id)
+	
 ## vim: set ts=4 : ##
